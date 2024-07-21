28e7858642ffc362659d0e081d757d9c5146ff2e
InnaHunchenko <InnaHunchenko@gmail.com>
2024-07-21 17:22:47 +0300

    Remove unnecessary from Program.cs in Products.Api

--- CoffeeShop.Server/CoffeeShop.Products.Api/Program.cs
+++ CoffeeShop.Server/CoffeeShop.Products.Api/Program.cs
@@ -1,46 +1,16 @@
 using CoffeeShop.Products.Api.Mapping;
 using CoffeeShop.Products.Api.Repository;
 using CoffeeShop.Products.Api.Services;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddGrpc();
-
 builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfile>(), [typeof(Program).Assembly], ServiceLifetime.Singleton);
 builder.Services.AddDbContext<DataContext>();
-
-var configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables()
-    .Build();
-
-builder.Services.AddSingleton<IConfiguration>(configuration);
-
-//builder.Host.ConfigureAutofac(Assembly.GetExecutingAssembly());
-
 builder.Services.AddHttpClient();
-
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
-
-//builder.Services.AddCors(options =>
-//{
-//    options.AddPolicy("AllowAll",
-//        builder =>
-//        {
-//            builder.AllowAnyOrigin()
-//                   .AllowAnyMethod()
-//                   .AllowAnyHeader();
-//        });
-//});
-
 var app = builder.Build();
-
-//app.UseCors("AllowAll");
-
-// Configure the HTTP request pipeline.
 app.MapGrpcService<ProductGrpcService>();
 ApplyMigration(app);
 app.Run();
