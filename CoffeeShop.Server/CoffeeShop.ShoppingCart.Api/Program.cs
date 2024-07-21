1c56647932dbe8615ca2c9f08c7fd4a7fe56f8af
InnaHunchenko <InnaHunchenko@gmail.com>
2024-07-21 17:23:35 +0300

    Remove unnecessary from Program.cs in ShoppingCart.Api

--- CoffeeShop.Server/CoffeeShop.ShoppingCart.Api/Program.cs
+++ CoffeeShop.Server/CoffeeShop.ShoppingCart.Api/Program.cs
@@ -1,11 +1,9 @@
 using System.Reflection;
 using CoffeeShop.Infrastructure;
-using Google.Protobuf.WellKnownTypes;
 using Grpc.Net.Client;
 using ProductsClient;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,25 +15,8 @@ builder.Services.AddSingleton(sp =>
     return new ProductService.ProductServiceClient(channel);
 });
 
-//builder.Services.AddGrpcClient<ProductService.ProductServiceClient>(options =>
-//{
-//    options.Address = new Uri("http://products-api:8080");
-//});
-
-//var channel = GrpcChannel.ForAddress("http://products-api:8080");
-//var client = new ProductService.ProductServiceClient(channel);
-//var emptyRequest = new Empty();
-//var categories = await client.GetCategoriesAsync(emptyRequest);
-
-//foreach (var item in categories.Categories)
-//{
-//    Console.WriteLine(item.Name);
-//}
-
 var app = builder.Build();
 
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
