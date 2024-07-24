6c35127d24893251ecb44248bb2b9fe9c6a60b96
InnaHunchenko <InnaHunchenko@gmail.com>
2024-07-24 12:31:38 +0300

    Remove grpc products.api project

--- CoffeeShop.Server/CoffeeShop.Products.Api/Models/Dto/CategoryDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CoffeeShop.Products.Api.Models.Dto
-{
-    public class CategoryDto
-    {
-        public string Name { get; set; }
-        public List<string> Subcategories { get; set; }
-    }
-}
